3d86f39f0ac3ea468105e1aa03e7a8d666ed19b5
Tuffik Kuri <tjkuri99@gmail.com>
2024-03-06 20:42:42 -0500

    Add basketball page - Empty component for basketball page - Add some basic routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import "./index.css"
 
 import {Home} from './components/Home'
+import {BasketBall} from './components/Basketball'
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div className='h-screen bg-spaceCadet text-verdigris'>
-    <Home />
+  <Router>
+    <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/basketball" element={<BasketBall/>} />
+    </Routes>
+  </Router>
   </div>
 );
