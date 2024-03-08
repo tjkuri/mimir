6a8c6aa43b2c7141902b3e45ff482a04c82a6d67
Tuffik Kuri <tjkuri99@gmail.com>
2024-03-07 19:44:55 -0500

    slight formatting changes to button

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ export class Home extends React.Component {
                         Renowned for his unparalleled wisdom, the most knowledgable giant takes on the Vegas line setters.
                     </p>
                     <Link to="/basketball">
-                        <button className="rounded-lg w-1/5 bg-bittersweet hover:bg-ghostWhite hover:text-verdigris">
+                        <button className="rounded-lg w-1/6 py-1 bg-bittersweet hover:bg-ghostWhite hover:text-verdigris">
                             <FontAwesomeIcon icon={faBasketball} />
                         </button>
                     </Link>
