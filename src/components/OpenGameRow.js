f2eb468090fa570f43e201882de74e0d98c825d8
Tuffik Kuri <tjkuri99@gmail.com>
2024-03-24 21:23:13 -0400

    Update game displays to reflect that the backend now extracts the exact DK line we want for NBA totals

--- src/components/OpenGameRow.js
+++ src/components/OpenGameRow.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 function getStatus (game) {
   console.log(game)
-  if (game.status == 'Final') { return 'Final'; }
   if (game.period == 0) { return 'Upcoming'; }
   return 'Qtr ' + game.period + ': ' + String(game.home_team_score) + ' - ' + String(game.visitor_team_score)
 }
@@ -10,24 +9,19 @@ function getStatus (game) {
 // TODO: Not sure if I want to move all of the (albiet very small) calculations here to the back end.
 // If I dont need to refactor this to use better practices, dont want all the magic strings
 export default function OpenGameRow({ game }) {
-  const status = getStatus(game)
-  let dkLine = false;
-  let o_u = false;
+  const statusString = getStatus(game)
+  let o_u = game.myLine >= game.draftkings_line ? 'O' : 'U';
   let play = false;
-  if (status != 'Final') { 
-    dkLine = game.bookmakers[0].markets[0].outcomes[0].point
-    o_u = game.myLine >= dkLine ? 'O' : 'U'
-  }
   
   return (
       <tr className='text-bittersweet' key={game.id}>
             <td className="px-4">{game.visitor_team.abbreviation} @ {game.home_team.abbreviation}</td>
-            <td className="px-4">{status}</td>
+            <td className="px-4">{statusString}</td>
             <td className="px-4">{game.myLine}</td>
-            <td className="px-4">{status == 'Final' ? '-' : dkLine}</td>
-            <td className="px-4">{status == 'Final' ? '-' : Math.abs(game.myLine - dkLine).toFixed(2)}</td>
-            <td className="px-4">{status == 'Final' ? '-' : o_u}</td>
-            <td className="px-4">{status == 'Final' ? '-' : play}</td>
+            <td className="px-4">{statusString == 'Final' ? '-' : game.draftkings_line}</td>
+            <td className="px-4">{statusString == 'Final' ? '-' : Math.abs(game.myLine - game.draftkings_line).toFixed(2)}</td>
+            <td className="px-4">{statusString == 'Final' ? '-' : o_u}</td>
+            <td className="px-4">{statusString == 'Final' ? '-' : play}</td>
       </tr>
   );
 }
