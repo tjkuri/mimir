cb9fd0913ccc5c427f935a235a0963b716d89eb0
Tuffik Kuri <tjkuri99@gmail.com>
2024-03-24 21:37:45 -0400

    Add/fix logic to determine whether our recommendation resulted in W or L

--- src/components/ClosedGameRow.js
+++ src/components/ClosedGameRow.js
@@ -2,14 +2,24 @@ import React from 'react';
 
 
 export default function ClosedGameRow({ game }) { 
+  let result = 'Unavailable'
+  if(game.draftkings_line){
+    const recommendOver = game.myLine >= game.draftkings_line
+    if(recommendOver){
+      result = game.home_team_score + game.visitor_team_score >= game.draftkings_line ? 'W' : 'L'
+    }
+    else{
+      result = game.home_team_score + game.visitor_team_score < game.draftkings_line ? 'W' : 'L'
+    }
+  }
   return (
       <tr className='text-bittersweet' key={game.id}>
             <td className="px-4">{game.visitor_team.abbreviation} @ {game.home_team.abbreviation}</td>
             <td className="px-4">{String(game.home_team_score) + ' - ' + String(game.visitor_team_score)}</td>
             <td className="px-4">{game.home_team_score + game.visitor_team_score}</td>
             <td className="px-4">{game.myLine}</td>
-            <td className="px-4">{game.draftkings_line ? game.draftkings_line : 'Couldnt find DK line'}</td>
-            <td className="px-4">{'TBA'}</td>
+            <td className="px-4">{game.draftkings_line ? game.draftkings_line : 'Unavailable'}</td>
+            <td className="px-4">{result}</td>
       </tr>
   );
 }
